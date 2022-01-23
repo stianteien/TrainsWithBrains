0134d9e0aab5c7ddc7389c3c0e13f7e6d96403c0
Stian Teien <stian.teien@gmail.com>
2022-01-23 17:28:21 +0100

    modified reward to get below 0

--- subwaysystem.py
+++ subwaysystem.py
@@ -161,8 +161,8 @@ class SubwaySystem:
             distances = pdist([train.position for train, _,_ in self.trains])
             self.state = np.append(self.state, distances)
             
-            print(distances)
-            self.reward += train.speed + distances
+            distance_reward = (distances[0]-50) if (distances[0]-50)<0 else 0
+            self.reward += train.speed + distance_reward
         
         self.counter += 1
         #self.reward = self.reward - self.counter
