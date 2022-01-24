f8cdaf4746f1a890bb5f19820b549c9cf1e03bf9
Stian Teien <stian.teien@gmail.com>
2022-01-24 23:20:34 +0100

    twick reward

--- subwaysystem.py
+++ subwaysystem.py
@@ -164,7 +164,7 @@ class SubwaySystem:
             distances = pdist([train.position for train, _,_ in self.trains])
             self.state = np.append(self.state, distances)
             
-            distance_reward = (distances[0]-100) if (distances[0]-100)<0 else 0
+            distance_reward = (distances[0]-75) if (distances[0]-75)<0 else 0
             self.reward += train.speed + distance_reward
         
         self.counter += 1
